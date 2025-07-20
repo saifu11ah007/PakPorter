ef38c7de432f8a53d7ebc32f1626491fd49fcc9b
saifu11ah007 <saifulah880@gmail.com>
2025-07-20 12:20:46 +0500

    Post wish backend deployed

--- backend/api/express.js
+++ backend/api/express.js
@@ -4,7 +4,7 @@ import express from 'express';
        import connectDB from '../config/db.js';
        import AuthRouter from '../routes/AuthRouter.js';
        import serverless from 'serverless-http';
-import WishRouter from '../controllers/WishController.js';
+import WishRouter from '../routes/WishRouter.js';
        dotenv.config();
        const app = express();
 
