2882102fad0a5e8c1cb67551ea043d6d463b9d87
saifu11ah007 <saifulah880@gmail.com>
2025-07-24 23:26:40 +0500

    Images link fix

--- backend/routes/WishRouter.js
+++ backend/routes/WishRouter.js
@@ -12,25 +12,9 @@ import { default as upload, saveProductImagesToBlob } from '../middleware/upload
 
 const router = express.Router();
 
-// Routes
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
-  if (!token) {
-    return res.status(401).json({ message: 'Authentication token required' });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: 'Invalid or expired token' });
-    }
-    req.user = user;
-    next();
-  });
-};
 
 // POST /wish - Create a new product wish
-router.post('/', authenticateToken,createWish, upload, saveProductImagesToBlob);
+router.post('/', authMiddleware,createWish, upload, saveProductImagesToBlob);
 router.get('/', getWishes); // Get all wishes
 router.get('/:id', getWishById); // Get a single wish by ID
 router.put('/:id', authMiddleware, updateWish); // Update a wish (authMiddlewareed)
