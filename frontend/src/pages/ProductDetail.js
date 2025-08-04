6cc33ee5ced293948b8fa6bc1227d410644a7f67
saifu11ah007 <saifulah880@gmail.com>
2025-08-04 12:59:42 +0500

    adding redirect fix

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -415,7 +415,7 @@ const WishDetailPage = () => {
                 <User className="w-5 h-5 text-gray-400" />
                 <span>Posted by {wish.createdBy.fullName}</span>
               </div>
-
+<h1></h1>
               <div className="flex items-center gap-3 text-gray-700">
                 <Clock className="w-5 h-5 text-gray-400" />
                 <span>
