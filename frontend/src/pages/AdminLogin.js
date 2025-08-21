f22d286e61be540206811f900831cd8a279813e6
saifu11ah007 <saifulah880@gmail.com>
2025-08-21 13:34:20 +0500

    Bid add page added fix

--- frontend/src/pages/AdminLogin.js
+++ frontend/src/pages/AdminLogin.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './AdminLogin.css';
 
 const AdminLogin = () => {
+  
   const [credentials, setCredentials] = useState({
     username: '',
     password: ''
